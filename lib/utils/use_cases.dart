50f7ee8273c6505389452007d27beec745db71ef
mickaelrei <mickael.reichert@gmail.com>
2023-11-18 00:03:18 -0300

    Added global use case for sale entity

--- lib/utils/use_cases.dart
+++ lib/utils/use_cases.dart
@@ -1,8 +1,13 @@
+import '../entities/partner_store.dart';
+import '../entities/sale.dart';
+import '../entities/user.dart';
 import '../entities/vehicle.dart';
 import '../repositories/partner_store_repository.dart';
+import '../repositories/sale_repository.dart';
 import '../repositories/user_repository.dart';
 import '../repositories/vehicle_repository.dart';
 import '../usecases/partner_store_use_case.dart';
+import '../usecases/sale_use_case.dart';
 import '../usecases/user_use_case.dart';
 import '../usecases/vehicle_use_case.dart';
 
@@ -15,6 +20,9 @@ late final PartnerStoreUseCase partnerStoreUseCase;
 /// Global use case for [Vehicle] objects
 late final VehicleUseCase vehicleUseCase;
 
+/// Global use case for [Sale] objects
+late final SaleUseCase saleUseCase;
+
 var _loaded = false;
 
 /// Method to initialize all global use cases
@@ -26,6 +34,8 @@ Future<void> initializeUseCases() async {
   partnerStoreUseCase = PartnerStoreUseCase(const PartnerStoreRepository());
 
   vehicleUseCase = const VehicleUseCase(VehicleRepository());
+
+  saleUseCase = const SaleUseCase(SaleRepository());
 
   // Set loaded
   _loaded = true;
